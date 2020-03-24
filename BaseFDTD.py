c4b7154cd380b830ea3e7717aa6ea15afa222421
fraserac <56896998+fraserac@users.noreply.github.com>
2020-03-24 11:02:29 +0000

    Add files via upload

--- BaseFDTD.py
+++ BaseFDTD.py
@@ -14,13 +14,15 @@ from Material_Def import *
 """
 
 import numpy as np
-import scipy as sci
 import math
 import matplotlib.pylab as plt
 import matplotlib.animation as animation
-from Material_Def import *
+#from Material_Def import *
 from scipy import signal as sign
 import sys 
+from decimal import *
+import itertools as it
+from scipy import signal as sign
 
   ##M MAKE THIS MORE ACCURATE LATER
 
@@ -83,145 +85,398 @@ for count in range(timeSteps):
 #EXPAND TO 2D after considering memory optimisation, then include particle sutff. 
 def sourceGen(T):
     #print(T)
-    pulse = np.sin(2*np.pi*freq_in*delT*T)#np.exp(-(T - 30)*(T-30)/100)# - (50))*(1/CharImp)
+    pulse = np.exp(-(T - 30)*(T-30)/100)# - (50))*(1/CharImp)
     #print(pulse)
     #pulse = np.exp(-((t-t0)/tau)**2)
     return pulse
 
-def sourceGen2(T):
+def sourceGen2(P, T):
     #deltaT =( 50*0.00033273660620797844)/(2*299462945.5871806) - 1.111111111111111e-12/2
-    pulse = np.sin(2*np.pi*freq_in*delT*T - (50+1))
+    pulse = np.sin(2*np.pi*P.freq_in*P.delT*T)
     #print(pulse)
     #pulse = np.exp(-((t-t0)/tau)**2)
     return pulse
 
 
 
-def FieldInit(Size, timeJumps):
+def FieldInit(V,P):
     while (True):
         try:
-            if type(Size) != int:
+            if type(P.Nz) != int:
                 raise TypeError('Grid spaces must be a positive integer value')
                 break
-            if type(timeJumps) != int:
+            if type(P.timeSteps) != int:
                 raise TypeError('Number of timeSteps must be positive integer valued')
                 break
-            if timeJumps > 2**14:
+            if P.timeSteps > 2**14:
                 raise ValueError('timeSteps max too large')
                 break
-            if Size > 250:
+            if P.Nz > 1000:
                 raise ValueError('Grid size too big')
                 break
-            if Size ==0:
+            if P.Nz ==0:
                 raise ValueError('Cannot have grid size 0!')
                 break
-            if timeJumps==0:
+            if P.timeSteps==0:
                 raise ValueError('Cannot have zero timeSteps!')
                 break
-            if Size < 0:
+            if P.Nz < 0:
                 raise ValueError('Grid size cannot be negative')
                 break
-            if timeJumps < 0:
+            if P.timeSteps < 0:
                 raise ValueError('Cannot have negative timeSteps')
                 break
             break
         except ValueError as e:
             print(e)
-            sys.exit()
-        
-    Ex =np.zeros(Size)#,dtype=complex)
-    Hy=np.zeros(Size)#,dtype=complex)
-    Ex_History= [[]]*timeJumps
-    Hy_History= [[]]*timeJumps
-    Hys = []*timeJumps
-    Ezs = []*timeJumps
-    return Ex, Ex_History, Hy, Hy_History,Hys, Ezs
-   
-    
-def SourceCalc(UpHyMat, UpExMat, Nz): # this function will run the FDTD over just the initial media and measure the points at x1 over t
+            sys.exit() 
+    V.Ex =np.zeros(P.Nz+1)#,dtype=complex)
+    V.Hy=np.zeros(P.Nz+1)#,dtype=complex)
+    V.Ex_History= [[]]*P.timeSteps
+    V.Hy_History= [[]]*P.timeSteps
+    V.Psi_Ex_History= [[]]*P.timeSteps
+    V.Psi_Hy_History= [[]]*P.timeSteps
+    V.Exs = []
+    V.Hys = []
+    return V.Ex, V.Ex_History, V.Hy, V.Hy_History, V.Psi_Ex_History, V.Psi_Hy_History, V.Exs, V.Hys
+
+
+def SmoothTurnOn(V,P):
+    ppw =  P.c0 /(P.freq_in*P.dz)
+    for timer in range(P.timeSteps):
+        if(timer*P.delT < P.period):
+            V.Exs.append(float(Decimal(np.sin(2.0*np.pi/ppw*(P.courantNo*timer)))))
+            V.Hys.append(float(Decimal(np.sin(2.0*np.pi/ppw*(P.courantNo*(timer+1))))))
+        elif(timer*P.delT >= P.period):  
+            V.Exs.append(0)
+            V.Hys.append(0)
+    for boo in range(P.timeSteps):
+        if(V.Hys[boo] ==0):
+          V.Hys[boo-1] =0
+        #if(V.Exs[boo] ==0):
+         # V.Exs[boo-1] =0
+          break    
+    return V.Exs, V.Hys   
+# FIX TURN OFF JITTER
+
+
+def EmptySpaceCalc(V,P): # this function will run the FDTD over just the initial media and measure the points at x1 over t
     #material to find transmission and reflection vals
-    UpHyMat = np.zeros(Nz) #THIS IS INITIALISER DON'T PASS THROUGH FOR LOOP
-    UpExMat = np.zeros(Nz)
+    V.UpHyMat = np.zeros(P.Nz+1) #THIS IS INITIALISER DON'T PASS THROUGH FOR LOOP
+    V.UpExMat = np.zeros(P.Nz+1)
     
-    UpHyBackground = (1/CharImp)*courantNo
-    UpExBackground = CharImp*courantNo
+    UpHyBackground = (1/P.CharImp)*P.courantNo
+    UpExBackground = P.CharImp*P.courantNo
 
 
-    for jl in range(0, Nz):  
-        UpExMat[jl] =UpExBackground
-        UpHyMat[jl] =UpHyBackground
-        
-    x1Loc = 80 # be careful with thin regions
-    return UpHyMat, UpExMat, x1Loc
-  
-def lossyMat():
-    #Calculate change to co-efficients
-    # eLoss = sigma_e dt, mLoss = sigma_m dt 
-    #for now explicit values
-    eLoss =0.001
-    mLoss = 0
-    eSelfCo = (1-eLoss)/(1+eLoss)#
-    eHcompsCo = 1+eLoss
-    return eSelfCo, EHcompsCo
+    for jl in range(0, P.Nz):  
+        V.UpExMat[jl] =UpExBackground
+        V.UpHyMat[jl] =UpHyBackground
+ 
+    return V.UpHyMat, V.UpExMat
+
+
+
+def Material(V,P):
+    for kj in range(P.Nz):
+            if(kj < P.materialFrontEdge):  
+                V.epsilon[kj] = 1
+                V.mu[kj] = 1
+                V.UpExHcompsCo[kj] =1
+                V.UpExSelf[kj] =1
+                V.UpHyEcompsCo[kj] =1
+                V.UpHySelf[kj] = 1
+            if(kj >= P.materialFrontEdge and kj < P.materialRearEdge):
+                V.epsilon[kj] = P.epsRe
+                V.mu[kj] = P.muRe
+                V.UpExHcompsCo[kj] = P.eHcompsCo
+                V.UpExSelf[kj] = P.eSelfCo
+                V.UpHyEcompsCo[kj] =P.hEcompsCo
+                V.UpHySelf[kj] = P.hSelfCo
+            if(kj>= P.materialRearEdge):
+                V.epsilon[kj] = 1
+                V.mu[kj] = 1 
+                V.UpExHcompsCo[kj] = 1
+                V.UpExSelf[kj] =1
+                V.UpHyEcompsCo[kj] =1
+                V.UpHySelf[kj] = 1
+    return V.epsilon, V.mu, V.UpExHcompsCo, V.UpExSelf, V.UpHyEcompsCo, V.UpHySelf
+
+    
+
 
 #PASS TO MASTER CONTROLLER, THEN THROUGH TO UPDATECOEF, WHERE THE NEW CO-EFF WILL DIVIDE uPEX AND UPHY at mat, 
 
-def UpdateCoef(UpHyMat, UpExMat, Nz):# POTENTIAL ISSUE, COURANT NO AND DOUBLE DEFINITION OF MU EPS.
+def UpdateCoef(V,P):# POTENTIAL ISSUE, COURANT NO AND DOUBLE DEFINITION OF MU EPS.
     #CHECK COURANT NO.
-    UpHyBackground = (1/CharImp)*courantNo
-    UpExBackground = CharImp*courantNo
-    print(UpExBackground)
-    UpHyMat = np.zeros(Nz) #THIS IS INITIALISER DON'T PASS THROUGH FOR LOOP
-    UpExMat = np.zeros(Nz)
-    for k in range(0, MaterialFrontEdge-1):  
-        UpExMat[k] =UpExBackground
-        UpHyMat[k] =UpHyBackground
-    for jj in range(MaterialFrontEdge-1, MaterialRearEdge-1):
-        UpExMat[jj] = (UpExBackground/MatEps)
-        UpHyMat[jj] = (UpHyBackground/MatMu)
-    for ii in range(MaterialRearEdge-1, Nz):
-        UpExMat[ii] = UpExBackground
-        UpHyMat[ii] = UpHyBackground
-   
-    return UpHyMat, UpExMat  
+    UpHyBackground = (1/P.CharImp)*P.courantNo
+    UpExBackground = P.CharImp*P.courantNo
+    UpHyMat = np.zeros(P.Nz+1) #THIS IS INITIALISER DON'T PASS THROUGH FOR LOOP
+    UpExMat = np.zeros(P.Nz+1)
+    #UpHySelf = np.zeros(P.Nz)
+    #UpExSelf = np.zeros(P.Nz)
+    
+    for k in range(P.Nz):
+        UpExMat[k]= UpExBackground/V.epsilon[k]
+        UpHyMat[k]= UpHyBackground/V.mu[k]
+
+    return UpHyMat, UpExMat
 
 
 
-def HyBC(Hy, size):
-    Hy[size-1] = Hy[size-2]
-    return Hy[size-1]
-   
+
 # FOR HY AND EX update/EZ? feed in eSelfCo and hSelfCo
-def HyUpdate(Hy, Ex, UpHyMat, size):
-    for nz in range(0, size-1):
-        Hy[nz] = Hy[nz] + (Ex[nz+1]-Ex[nz])*UpHyMat[nz]
-    return Hy[0:size-2]
 
-def HyTfSfCorr(HyTfsf, counter, UpHyMatTfsf, Ezs):
-     HyTfsf -= Ezs[counter]*UpHyMatTfsf#*np.exp(-(counter - 30)*(counter-30)/100)
+
+def HyTfSfCorr(V, P, counts):
+     #V.Hy[P.nzsrc-1] -= sourceGen(counts)
+     V.Hy[P.nzsrc-1] -= V.Exs[counts]/P.CharImp#*np.exp(-(counter - 30)*(counter-30)/100)
      #link to sourceGen for harmonic or ricker or gaussian etc 
      #np.sin((2*np.pi)/Nlam*(courantNo))
-     return HyTfsf
-   
-def ExBC(Ex, size):
-    Ex[0] = Ex[1]
-    Ex[size-1]=  Ex[size-2]
-    return Ex[0], Ex[size-1]
+     return V.Hy[P.nzsrc-1]
    
 
-def ExUpdate(Ex, UpExMat, Hy,  size):
-    for nz in range(1, size-1):
-        Ex[nz] = Ex[nz] + (Hy[nz]-Hy[nz-1])*UpExMat[nz]#*UpExMat[nz]
-    return Ex[1:size-2]    
+   
 
-
-def ExTfSfCorr(ExTfsf, counter, nzsrc, UpExMatTfsf, Hys):
-   # ExTfsf= ExTfsf + np.exp(-(counter +0.5 -(-0.5)-30)*(counter +0.5 -(-0.5)-30)/100)
-    ExTfsf += Hys[counter]# *np.exp(-(counter +0.5 -(-0.5)-30)*(counter +0.5 -(-0.5)-30)/100)
-    return ExTfsf
+def ExTfSfCorr(V,P, counts):
+    #V.Ex[P.nzsrc] += sourceGen2(P, counts)
+    V.Ex[P.nzsrc] += V.Hys[counts]# *np.exp(-(counter +0.5 -(-0.5)-30)*(counter +0.5 -(-0.5)-30)/100)
+    return V.Ex[P.nzsrc]
 
 
 
+##### CPML STUFF 
+    
+def CPML_FieldInit(V,P, C_V, C_P):#INITIALISE FIELD PARAMS 
+    C_V.kappa_Ex =np.zeros(P.Nz+1)
+    C_V.kappa_Hy = np.zeros(P.Nz+1)
+    C_V.psi_Ex =np.zeros(P.Nz+1)
+    C_V.psi_Hy = np.zeros(P.Nz+1)
+    C_V.alpha_Ex= np.zeros(P.Nz+1)
+    C_V.alpha_Hy= np.zeros(P.Nz+1)
+    C_V.sigma_Ex =np.zeros(P.Nz+1)   # specific spatial value of conductivity 
+    C_V.sigma_Hy = np.zeros(P.Nz+1)
+    C_V.beX =np.zeros(P.Nz+1)#np.exp(-(sigma_Ex/(permit_0*kappa_Ex) + alpha_Ex/permit_0 )*delT)
+    C_V.bmY =np.zeros(P.Nz+1)#np.exp(-(sigma_Hy/(permea_0*kappa_Hy) + alpha_Hy/permea_0 )*delT)
+    C_V.ceX = np.zeros(P.Nz+1)
+    C_V.cmY = np.zeros(P.Nz+1)
+    C_V.eLoss_CPML = np.zeros(P.Nz+1)
+    C_V.mLoss_CPML = np.zeros(P.Nz+1)
+    C_V.Ca = np.zeros(P.Nz+1)
+    C_V.Cb = np.zeros(P.Nz+1)
+    C_V.Cc = np.zeros(P.Nz+1)
+    C_V.C1 = np.zeros(P.Nz+1)
+    C_V.C2 = np.zeros(P.Nz+1)
+    C_V.C3 = np.zeros(P.Nz+1)
+    
+    return C_V
+    
+def CPML_ScalingCalc(V, P, C_V, C_P):
+    jj=P.pmlWidth
+    jjj = P.pmlWidth-1
+    for nz in range(P.Nz):   #SCALES VARS AWAY FROM SIM DOMAIN, REVERSE FOR LOOP
+        nz1= nz+1
+        if (nz1 <=P.pmlWidth): 
+            C_V.kappa_Ex[nz] =1+(C_P.kappaMax-1)*((P.pmlWidth-nz1)/(P.pmlWidth-1))**C_P.r_scale
+            
+            C_V.sigma_Ex[nz] = C_P.sigmaOpt*((P.pmlWidth - nz1)/(P.pmlWidth-1))**C_P.r_scale   #np.abs((((np.abs(nz -0.75))/P.pmlWidth)**(C_P.r_scale))*C_P.sigmaEMax)
+            
+            C_V.alpha_Ex[nz] = C_P.alphaMax*(nz1/(P.pmlWidth-1))**C_P.r_a_scale #np.abs((1-((counter)/P.pmlWidth)**C_P.r_a_scale)*C_P.alphaMax)
+            
+            
+        elif nz >= P.Nz+2 -(P.pmlWidth):
+            C_V.kappa_Ex[nz] = C_V.kappa_Ex[jj]
+            
+            C_V.sigma_Ex[nz] = C_V.sigma_Ex[jj]
+            
+            C_V.alpha_Ex[nz] = C_V.alpha_Ex[jj]
+            jj-=1
+            
+        else:
+            C_V.kappa_Ex[nz] = 1
+            
+            C_V.sigma_Ex[nz] = 0
+            
+            C_V.alpha_Ex[nz] = 0
+            
+    #for nz in range(P.Nz-1):   
+        
+        if (nz1 <=P.pmlWidth-1):
+            C_V.kappa_Hy[nz] =1+(C_P.kappaMax-1)*((P.pmlWidth-nz1-0.5)/(P.pmlWidth-1))**C_P.r_scale
+            C_V.sigma_Hy[nz] = C_P.sigmaOpt*((P.pmlWidth -nz1-0.5)/(P.pmlWidth-1))**C_P.r_scale   #np.abs(((C_P.sigmaEMax)*(np.abs(nz -0.75)/P.pmlWidth)**(C_P.r_scale))*C_P.sigmaHMax)
+            C_V.alpha_Hy[nz] = C_P.alphaMax*(nz1-0.5/(P.pmlWidth-1))**C_P.r_a_scale#np.abs(((1)*(1-(counter)/P.pmlWidth)**(C_P.r_a_scale))*C_P.alphaMax)
+            
+        
+        elif nz >= P.Nz +2 - P.pmlWidth:
+            C_V.kappa_Hy[nz] = C_V.kappa_Hy[jjj]
+            
+            C_V.sigma_Hy[nz] = C_V.sigma_Hy[jjj]
+            
+            C_V.alpha_Hy[nz] = C_V.alpha_Hy[jjj]
+            jjj-=1
+        else:
+            C_V.kappa_Hy[nz] = 1
+            
+            C_V.sigma_Hy[nz] = 0
+            
+            C_V.alpha_Hy[nz] = 0 
+        
+    return C_V.sigma_Ex, C_V.sigma_Hy, C_V.alpha_Ex,  C_V.alpha_Hy, C_V.kappa_Ex, C_V.kappa_Hy
+
+    
+def CPML_Ex_RC_Define(V, P, C_V, C_P):
+    
+    jj = P.pmlWidth-1
+    jjj =P.pmlWidth-1
+    for nz in range(0, P.Nz):
+        if(nz <=P.pmlWidth-1):
+            
+            C_V.beX[nz] = np.exp(-((C_V.sigma_Ex[nz]*P.delT/C_V.kappa_Ex[nz])+((C_V.alpha_Ex[nz]*P.delT)/P.permit_0)))
+        
+        elif nz >= P.Nz+2 -P.pmlWidth:
+            C_V.beX[nz] = np.exp(-((C_V.sigma_Ex[nz]*P.delT/C_V.kappa_Ex[nz])+((C_V.alpha_Ex[nz]*P.delT)/P.permit_0)))
+            jj-=1
+        else:
+            C_V.beX[nz] =0
+            
+            
+            
+        if C_V.sigma_Ex[nz] ==0 and nz==P.pmlWidth-1 and C_V.alpha_Ex[nz]==0:
+           C_V.ceX[nz]=0
+        elif nz <=P.pmlWidth-1:
+            C_V.ceX[nz] = (C_V.beX[nz]-1)*C_V.sigma_Ex[nz]/(C_V.sigma_Ex[nz]+C_V.alpha_Ex[nz]*C_V.kappa_Ex[nz])/C_V.kappa_Ex[nz]
+        elif nz >= P.Nz+2 -P.pmlWidth:
+            C_V.ceX[nz] = (C_V.beX[jjj]-1)*C_V.sigma_Ex[jjj]/(C_V.sigma_Ex[jjj]+C_V.alpha_Ex[jjj]*C_V.kappa_Ex[jjj])/C_V.kappa_Ex[jjj]
+            jjj-=1
+        else:
+             C_V.ceX[nz] =0
+            
+    return C_V.beX, C_V.ceX
+
+
+def CPML_HY_RC_Define(V, P, C_V, C_P):
+    jj = P.pmlWidth-2
+    jjj =P.pmlWidth-2
+   
+    for nz in range(0, P.Nz):
+        
+        if(nz <=P.pmlWidth-2):
+            C_V.bmY[nz] = np.exp(-(C_V.sigma_Hy[nz]/(C_V.kappa_Hy[nz]+C_V.alpha_Hy[nz]))*P.delT/P.permea_0)
+        elif nz >= P.Nz+2 -P.pmlWidth:
+            C_V.bmY[nz] = np.exp(-(C_V.sigma_Hy[jj]/(C_V.kappa_Hy[jj]+C_V.alpha_Hy[jj]))*P.delT/P.permea_0)
+            jj-=1
+        else:
+            C_V.bmY[nz] =0 
+            
+             
+        if C_V.sigma_Hy[nz] ==0 and nz==P.pmlWidth-1 and C_V.alpha_Hy[nz]==0:
+           C_V.cmY[nz]=0
+        elif nz<=P.pmlWidth-1:
+            C_V.cmY[nz] = (C_V.bmY[nz]-1)*C_V.sigma_Hy[nz]/(C_V.sigma_Hy[nz]+C_V.alpha_Hy[nz]*C_V.kappa_Hy[nz])/C_V.kappa_Hy[nz]
+        elif nz >= P.Nz+2 -P.pmlWidth:
+            C_V.cmY[nz] = (C_V.bmY[jjj]-1)*C_V.sigma_Hy[jjj]/(C_V.sigma_Hy[jjj]+C_V.alpha_Hy[jjj]*C_V.kappa_Hy[jjj])/C_V.kappa_Hy[jjj]
+            jjj-=1
+        else:
+             C_V.cmY[nz] =0    
+            
+        #C_V.cmY[P.Nz-1] = C_V.cmY[P.Nz-2]
+    return C_V.bmY, C_V.cmY
+
+def CPML_Ex_Update_Coef(V,P, C_V, C_P):
+    for nz in range(0, P.Nz-1):
+        C_V.eLoss_CPML[nz] = (C_V.sigma_Ex[nz]*P.delT)/(2*P.permit_0)
+        
+        C_V.Ca[nz] = V.UpExSelf[nz]#(1-C_V.eLoss_CPML[nz])/(1+C_V.eLoss_CPML[nz])
+        C_V.Cb[nz] =V.UpExHcompsCo[nz]*V.UpExMat[nz]#P.delT/P.permit_0/(1+C_V.eLoss_CPML[nz])
+        C_V.Cc[nz] = P.delT/((1+C_V.eLoss_CPML[nz])*P.permit_0)
+        
+    #for nz in range(P.pmlWidth, P.Nz-P.pmlWidth-1 ):
+       # C_V.Ca[nz] = V.UpExSelf[nz]
+        #C_V.Cb[nz] = V.UpExHcompsCo[nz]
+        
+    return C_V.eLoss_CPML, C_V.Ca, C_V.Cb, C_V.Cc    
+
+
+def CPML_Hy_Update_Coef(V,P, C_V, C_P):
+    for nz in range(0, P.Nz-2):
+        #C_V.mLoss_CPML[nz] = (C_V.sigma_Hy[nz]*P.delT)/(2*P.permea_0)
+        
+        C_V.C1[nz] =1#V.UpHySelf[nz] #(1-C_V.mLoss_CPML[nz])/(1+C_V.mLoss_CPML[nz])
+        C_V.C2[nz] = -P.delT/P.permea_0# V.UpHyEcompsCo[nz]#P.delT/((1+C_V.mLoss_CPML[nz])*P.permea_0*C_V.kappa_Hy[nz]*P.dz)
+        C_V.C3[nz] = P.delT/  ((1+C_V.mLoss_CPML[nz])*P.permea_0)#NOT CURRENTLY USED
+    #for nz in range(P.pmlWidth ,P.Nz-P.pmlWidth-1): 
+       # C_V.C1[nz] = V.UpHySelf[nz]
+       # C_V.C2[nz] = V.UpHyEcompsCo[nz]
+        
+    return C_V.mLoss_CPML, C_V.C1, C_V.C2, C_V.C3   
+    
+def denominators(V, P, C_V, C_P):
+    jj = P.pmlWidth-2
+    for j in range(0,P.Nz): 
+        if j <= P.pmlWidth -2:
+            C_V.den_Hydz[j]= 1/(C_V.kappa_Hy[j])
+        elif j>= P.Nz+2-P.pmlWidth:
+            C_V.den_Hydz[j] = 1/(C_V.kappa_Hy[jj])
+            
+            jj-=1
+        else:
+            C_V.den_Hydz[j] = 1/(1)
+    jj = P.pmlWidth-1
+    for j in range(0,P.Nz): 
+        if j <= P.pmlWidth-1:
+            C_V.den_Exdz[j]= 1/(C_V.kappa_Ex[j])
+        elif j>= P.Nz+2-P.pmlWidth:
+            C_V.den_Exdz[j] = 1/(C_V.kappa_Ex[jj])
+            jj-=1
+        else:
+            C_V.den_Exdz[j] = 1/(1)        
+    return C_V.den_Exdz, C_V.den_Hydz
+    
+    
+
+
+def CPML_Psi_e_Update(V,P, C_V, C_P):   # recursive convolution for E field REF
+    for nz in range(1, P.Nz-1): 
+        C_V.psi_Ex[nz] = C_V.beX[nz]*C_V.psi_Ex[nz] + C_V.ceX[nz]*(V.Hy[nz]-V.Hy[nz-1])
+        V.Ex[nz] = V.Ex[nz] + C_V.Cb[nz]*C_V.psi_Ex[nz]
+    
+    return C_V.psi_Ex, V.Ex 
+
+
+def CPML_Psi_m_Update(V,P, C_V, C_P):   # recursive convolution for H field REF
+    for nz in range(0, P.Nz-2): 
+        C_V.psi_Hy[nz] = C_V.bmY[nz]*C_V.psi_Hy[nz] + C_V.cmY[nz]*(V.Ex[nz]-V.Ex[nz+1])
+        V.Hy[nz] = V.Hy[nz] + C_V.C2[nz]*C_V.psi_Hy[nz]
+    
+    return C_V.psi_Hy, V.Hy 
+
+
+
+def CPML_HyUpdate(V,P, C_V, C_P):
+    for nz in range(0, P.Nz-2):
+        V.Hy[nz] = V.Hy[nz]*V.UpHySelf[nz] + (V.Ex[nz+1]-V.Ex[nz])*V.UpHyMat[nz]*V.UpHyEcompsCo[nz]*C_V.den_Hydz[nz]
+        #V.Hy[nz]= C_V.C1[nz]*V.Hy[nz]+C_V.C2[nz]*(V.Ex[nz]-V.Ex[nz+1])*C_V.den_Hydz[nz]
+    return V.Hy
+
+
+def CPML_ExUpdate(V,P, C_V, C_P):
+    for nz in range(1, P.Nz-1):
+        #print(nz)
+        #print("************************** ")
+        #breakpoint()
+        V.Ex[nz] = V.Ex[nz]*V.UpExSelf[nz] + (V.Hy[nz]-V.Hy[nz-1])*V.UpExHcompsCo[nz]*V.UpExMat[nz]*C_V.den_Exdz[nz]
+        #V.Ex[nz]= C_V.Ca[nz]*V.Ex[nz]+C_V.Cb[nz]*(V.Hy[nz-1]-V.Hy[nz])*C_V.den_Exdz[nz]
+    return V.Ex
+
+
+def CPML_PEC(V, P, C_V, C_P):
+    V.Ex[0] =0
+   # V.Ex[P.Nz-1]=0
+    return V.Ex[0], V.Ex[P.Nz-1]
+
+def CPML_PMC(V,P,C_V, C_P):
+    V.Hy[P.Nz-1]=0
+    return V.Hy[P.Nz-1]
 """
 Issue: feed fields back and forth. RETURN uphy etc etc , call
 """
--- Material_Def.py
+++ Material_Def.py
@@ -4,15 +4,9 @@
 """
 import numpy as np
 import scipy.constants as sci
+import sys
 #from MasterController import *
 
-
-UpHyMat =[]
-UpExMat =[]
-Ex =[]#,dtype=complex)
-Hy=[]#,dtype=complex)
-Ex_History= [[]]
-Hy_History=[[]]
 
 permit_0 = sci.epsilon_0
 permea_0 = sci.mu_0
@@ -22,57 +16,128 @@ muRe = 1
 muIm = 0
 
 c0 = sci.speed_of_light
-freq_in = 4e9
+freq_in = 1e6
 
 ### WILL NEED MAX FREQ WHEN HIGHER HARMONICS ARE PRESENT
-lamMin = c0/(np.sqrt(abs(epsRe)*abs(muRe))*freq_in)
-Nlam = 20
+lamMin = c0/freq_in
+Nlam = 20*np.sqrt(epsRe*muRe)
 dz =lamMin/Nlam  
-delT = (dz)/(c0)   # LOOK INTO HOW DZ AND DELT ARE USED, COURANT NO?
-courantNo = c0*delT/dz
+courantNo = 1   # LOOK INTO 2D VERSION
+delT = (courantNo*dz)/(c0)
+  # LOOK INTO HOW DZ AND DELT ARE USED, COURANT NO?
+
 CharImp =np.sqrt(permea_0)/np.sqrt(permit_0)
-MaterialFrontEdge = 100  # Discrete tile where material begins (array index)
-MaterialRearEdge = 130
+
+period = 1/freq_in
+
+pmlWidth =60
+domainSize = 200
+dimen =1
 
 
-Nz = 200   #Grid size
-
-timeSteps =2**8
+Nz = domainSize +2*dimen*pmlWidth   #Grid size
+minRange = 2000
+maxRange = 10000
+for N in range(minRange,maxRange):
+    check = (freq_in*N)/(1/delT)
+    if check/int(check) ==1:
+        timeSteps = N 
+        break
+    elif N == maxRange-1:
+       print('Could not find timestep that allowed freq_in to fall on an integer frequency bin index with range provided.') 
+       sys.exit()
+print('timesteps: ', timeSteps)
 t=np.arange(0, timeSteps, 1)*(delT)  # FOR VERIFICATION PLOTTING, EVALUATE IN CLASS
 
-nzsrc = 50 # Position of source 
+nzsrcFromPml = 20
+nzsrc = nzsrcFromPml + pmlWidth # Position of source 
+x2LocBehindSrc = 10
+if(x2LocBehindSrc >= nzsrcFromPml):
+    print('The probe for fft is in the PML region')
+    sys.exit()
+x1Loc = nzsrc+int((Nz-nzsrc)/2)
+x2Loc = nzsrc - (nzsrcFromPml-x2LocBehindSrc)
+MaterialDistFromPml = 70
+MaterialFrontEdge = MaterialDistFromPml + pmlWidth  # Discrete tile where material begins (array index)
+MaterialWidth = Nz - MaterialFrontEdge
+MaterialRearEdge = MaterialFrontEdge + MaterialWidth
+eLoss =0   # sigma e* delT/2*epsilon
+mLoss = 0
+eSelfCo = (1-eLoss)/(1+eLoss)#
+eHcompsCo = 1/(1+eLoss)
+hSelfCo = (1-mLoss)/(1+mLoss)
+hEcompsCo = 1/(1+mLoss)
 
 
 
 x1ColBe=[[]]*timeSteps 
 x1ColAf=[[]]*timeSteps
+UpHySelf= np.ones(Nz)
+UpHyEcompsCo = np.ones(Nz)
+UpExSelf = np.ones(Nz)
+UpExHcompsCo =np.ones(Nz)
+UpExMat =np.zeros(Nz)
+UpHyMat = np.zeros(Nz)
+Ex =np.zeros(Nz)#,dtype=complex)
+Hy=np.zeros(Nz)#,dtype=complex)
+Ex_History= [[]]*timeSteps
+Hy_History= [[]]*timeSteps
+Psi_Ex_History= [[]]*timeSteps
+Psi_Hy_History= [[]]*timeSteps
+Hys = []
+Exs = []
 
-class Params(object):
-    def __init__(self, epsRe, muRe, f_in, lMin, nlm, dz, dt, crntNo, matRear, matFront, gridNo, timeSteps):
-        self.permit_0 = 8.854187817620389e-12#
-        self.permea_0 =1.2566370614359173e-06
-        self.c0 = 299792458.0
-        self.epsRe = epsRe
-        self.muRe = muRe
-        self.freq_in = f_in
-        self.lamMin = lMin
-        self.Nlam = Nlm
-        self.dz = dz
-        self.delT = dt
-        self.courantNo= crntNo
-        self.MaterialFrontEdge = matFront
-        self.MaterialRearEdge = matRear
-        self.Nz = gridNo
-        self.timeSteps = timeSteps
-    
-    def __repr__(self):
-        return (f'{self.__class__.__name__}(', "Real Epsilon:",f'{self.epsRe!r}, "Real Mu", 'f'{self.muRe!r})
-    
-    def __str__(self):
-        return 'Class containing all values that remain constant throughout a sim'
-    
+epsilon = np.ones(Nz)
+mu = np.ones(Nz)
+
+
+
+###PML STUFF PARAMS
+
+kappaMax =15 # 'Stretching co-ordinate of pml, to minimise numerical dispersion set it as 1' : DOI: 10.22190/FUACR1703229G see conclusion
+r_scale = 5.4# Within ideal bounds see Journal of ELECTRICAL ENGINEERING, VOL 68 (2017), NO1, 47–53, see paragraph under eqn. 17 (scaling power is called 'm' )
+r_a_scale=1
+sigmaEMax =1.4*(0.8*(r_scale+1)/(dz*(permea_0/permit_0)**0.5))#1.1*sigmaOpt # Within ideal bounds for value, : Journal of ELECTRICAL ENGINEERING, VOL 68 (2017), NO1, 47–53, see paragraph under eqn. 17
+sigmaHMax = sigmaEMax#1.1*sigmaOpt # See International Journal of Computer Science and Network Security, VOL.18 No.12, December 2018, page 4 right hand side.
+sigmaOpt  =sigmaEMax
+#Optimal value of pml conductivity at far end of pml: DOI: 10.22190/FUACR1703229G see equation 13
+
+
+alphaMax= 0.24 # with bounds of ideal cpml alpha max, complex frequency shift parameter, Journal of ELECTRICAL ENGINEERING, VOL 68 (2017), NO1, 47–53, see paragraph under eqn. 17
+
+
+# VARIABLES
+kappa_Ex =np.zeros(Nz)
+kappa_Hy = np.zeros(Nz)
+psi_Ex =np.zeros(Nz)
+psi_Hy = np.zeros(Nz)
+alpha_Ex= np.zeros(Nz)
+alpha_Hy= np.zeros(Nz)
+sigma_Ex =np.zeros(Nz)   # specific spatial value of conductivity 
+sigma_Hy = np.zeros(Nz)
+beX =np.zeros(Nz)#
+bmY =np.zeros(Nz)#np.exp(-(sigmaHy/(permea_0*kappa_Hy) + alpha_Hy/permea_0 )*delT)
+ceX = np.zeros(Nz)
+cmY = np.zeros(Nz)
+
+Ca = np.zeros(Nz)
+Cb = np.zeros(Nz)
+Cc = np.zeros(Nz)
+C1 = np.zeros(Nz)
+C2 = np.zeros(Nz)
+C3 = np.zeros(Nz)
+
+eLoss_CPML =np.zeros(Nz)   # sigma e* delT/2*epsilon
+mLoss_CPML = np.zeros(Nz)
+den_Hydz = np.zeros(Nz)
+den_Exdz = np.zeros(Nz)    
     #set up class for FDTD parameters, so mastercontroller is unit testable
     
-    
+"""    
+m = 3; ma = 1 ; 
+sigZmax =  (0.8*(m+1)/(dxyz*(mu0/eps0*epsR)^0.5)); 
+aZmax = 0.05; 
+kZmax = 1.0; 
+"""
     
     
