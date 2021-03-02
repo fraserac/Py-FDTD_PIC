bdb641f43d216d2ddfd87b9d6aef9484e507d6c3
Fraser Cripps <56896998+fraserac@users.noreply.github.com>
2021-03-02 18:33:07 +0100

    Add files via upload

---
+++ function plotter.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 19:57:53 2020
+FUNCTION PLOT
+@author: Owner
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.close('all')
+
+gammaE = 2*np.pi*20e9*0.1
+omega_0E= 2*np.pi*20e9
+
+freq_in = np.arange(1e8, 5e13, 1e10)
+
+eps8= 1.5
+epsS =3
+plasmaFreqE = ((1.5)*omega_0E**2)
+pi = np.pi
+epsNum = plasmaFreqE
+epsDom = (omega_0E**2-(2*pi*freq_in*2*pi*freq_in) -2j*gammaE*2*pi*freq_in)
+
+epsDom2 =(omega_0E**2-(4*pi*freq_in*4*pi*freq_in) -2j*gammaE*4*pi*freq_in)
+
+epsilon2 = 1+ epsNum/(epsDom**2*epsDom2)
+epsilon =  1+epsNum/epsDom
+
+plt.plot(freq_in, np.real(epsilon))
+plt.plot(freq_in, np.imag(epsilon))
